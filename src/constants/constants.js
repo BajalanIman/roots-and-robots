e5761c59a8dda042f5ee5ae2c1e31defab4c3f48
Iman <bajalaniman@gmail.com>
2025-09-10 19:24:13 +0200

    Changing base url to railway

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "marcobackend-production.up.railway.app/";
-export const BASE_URL = "http://localhost:8800/";
+export const BASE_URL = "roots-and-robots-backend-production.up.railway.app/";
+//export const BASE_URL = "http://localhost:8800/";
